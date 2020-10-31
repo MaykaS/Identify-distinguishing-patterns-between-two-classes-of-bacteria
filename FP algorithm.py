ad49d76357f3ef5142808c323a6091a48eba1bea
Maya Sagalin <mayasag10@gmail.com>
2020-10-31 12:13:11 +0200

    fixed problems and added comments

--- FP algorithm.py
+++ FP algorithm.py
@@ -1,35 +1,38 @@
 import fpGrowth
 
+print("start of Listing 12.2")
 rootNode = fpGrowth.treeNode('pyramid',9,None)
 rootNode.children['eye'] = fpGrowth.treeNode('eye',13,None)
 rootNode.disp()
 rootNode.children['phoenix'] = fpGrowth.treeNode('phoenix', 3, None)
 rootNode.disp()
-
-
-
-
-#reload(fpGrowth)
+print("end of Listing 12.2")
+print("\n")
+print("start of Listing 12.3")
 simpDat = fpGrowth.loadSimpDat()
-print(simpDat)
-
+for i in simpDat:
+    print(simpDat)
+print("\n")
 initSet = fpGrowth.createInitSet(simpDat)
 print(initSet)
-
+print("\n")
 #The FP-tree
 myFPtree, myHeaderTab = fpGrowth.createTree(initSet, 3)
 myFPtree.disp()
-
-#reload(fpGrowth)
-fpGrowth.findPrefixPath('x', myHeaderTab['x'][1])
-fpGrowth.findPrefixPath('z', myHeaderTab['z'][1])
-fpGrowth.findPrefixPath('r', myHeaderTab['r'][1])
-
-reload(fpGrowth)
+print("end of Listing 12.3")
+print("\n")
+print("start of Listing 12.4")
+print(fpGrowth.findPrefixPath('x', myHeaderTab['x'][1]))
+print(fpGrowth.findPrefixPath('z', myHeaderTab['z'][1]))
+print(fpGrowth.findPrefixPath('r', myHeaderTab['r'][1]))
+print("end of Listing 12.4")
+print("\n")
+print("start of Listing 12.5") #TODO: DOESNT PRINT RIGHT!
 #Now create an empty list to store all the frequent itemsets
 freqItems = []
-fpGrowth.mineTree(myFPtree, myHeaderTab, 3, set([]), freqItems)
-freqItems
+print(fpGrowth.mineTree(myFPtree, myHeaderTab, 3, set([]), freqItems))
+print(freqItems)
 
+print("end of Listing 12.5")
 # TODO: Code to access the Twitter Python library - NEED?
 
